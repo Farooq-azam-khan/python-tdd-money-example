462470da455d29a480fbcf36d62b9ff8ae13ea0d
Farooq <farooq1.khan@ryerson.ca>
2022-01-08 12:45:00 -0500

    duplication pt.3/2

--- dollar.py
+++ dollar.py
@@ -2,6 +2,6 @@ class Dollar:
     def __init__(self, amount: float):
         self.amount = amount
     def times(self, multiplyer: float) -> None:
-        self.amount = self.amount * multiplyer 
+        self.amount *=  multiplyer 
     
 
