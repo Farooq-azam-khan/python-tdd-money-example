d1a970385d75ec6d1c29bc3923bd0945fa0342cf
Farooq <farooq1.khan@ryerson.ca>
2022-01-08 13:26:15 -0500

    moved eq to super class

--- dollar.py
+++ dollar.py
@@ -1,18 +1,19 @@
 class Money:
    def __init__(self, amount: float):
         self.amount = amount
+    
+    def equals(self, other):
+        return self.amount == other.amount 
+
+    def __eq__(self, other):
+        return self.equals(other)
  
 class Dollar(Money):
    
     def times(self, multiplyer: float):
         return Dollar(self.amount * multiplyer)
 
-    def equals(self, other):
-        return self.amount == other.amount 
-
-    def __eq__(self, other):
-        return self.equals(other)
-    
+   
 class Franc:
     def __init__(self, amount):
         self.amount = amount 
