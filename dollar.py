003fef588fe20a8786db370cdd45f3aa04a6f8e5
Farooq <farooq1.khan@ryerson.ca>
2022-01-08 13:20:30 -0500

    shameless duplication

--- dollar.py
+++ dollar.py
@@ -1,12 +1,26 @@
 class Dollar:
     def __init__(self, amount: float):
         self.amount = amount
-    def times(self, multiplyer: float) -> None:
+    
+    def times(self, multiplyer: float):
         return Dollar(self.amount * multiplyer)
 
     def equals(self, other):
         return self.amount == other.amount 
+
     def __eq__(self, other):
         return self.equals(other)
     
+class Franc:
+    def __init__(self, amount):
+        self.amount = amount 
+
+    def times(self, multiplier):
+        return Franc(self.amount * multiplier)
+    
+    def equals(self, other):
+        return self.amount == other.amount
+
+    def __eq__(self, other):
+        return self.equals(other)
 
--- test_money_example.py
+++ test_money_example.py
@@ -1,4 +1,4 @@
-from dollar import Dollar
+from dollar import Dollar, Franc
 
 def test_multiplication():
     five = Dollar(5)
@@ -10,3 +10,8 @@ def test_equality():
     assert Dollar(5).equals(Dollar(5))
     # Triangulation: when the second test demands a more general solution, then and only then do we generalize
     assert not Dollar(5).equals(Dollar(6))
+
+def test_franc_multiplication():
+    five = Franc(5)
+    assert Franc(10) == five.times(2)
+    assert Franc(15) == five.times(3)
