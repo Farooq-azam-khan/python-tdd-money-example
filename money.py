648b4ed80ea3b8e058ed15cb546fec9db7c65240
Farooq <farooq1.khan@ryerson.ca>
2022-01-08 14:18:03 -0500

    added currency abstract + small Money.franc interruption

--- money.py
+++ money.py
@@ -3,7 +3,8 @@ from abc import ABC, abstractmethod
 class Money(ABC):
     def __init__(self, amount: int):
         self.amount = amount
-     
+        self.currency = None 
+
     def __eq__(self, other):
         self_class_name = type(self).__name__
         other_class_name = type(other).__name__
@@ -12,6 +13,8 @@ class Money(ABC):
     @staticmethod
     def dollar(amount: int):
         return Dollar(amount) 
+    
+    
     @staticmethod
     def franc(amount: int):
         return Franc(amount)
@@ -23,24 +26,37 @@ class Money(ABC):
 
     
     @abstractmethod
-    def currency(self): # -> 'str'
+    def get_currency(self): # -> 'str'
         pass 
+
+    def currency(self):
+        return self.currency
 
 
 class Dollar(Money):
-   
+  
+    def __init__(self, amount: int, currency: str = "USD") -> None:
+        Money.__init__(self, amount)
+        self.currency = currency 
+
+
     def times(self, multiplyer: int):
         return Dollar(self.amount * multiplyer)
-    
-    def currency(self):
-        return "USD"
+
+    def get_currency(self) -> str:
+        return self.currency 
+
 
 class Franc(Money):
 
+    def __init__(self, amount: int, currency: str = "CHF") -> None:
+        Money.__init__(self, amount)
+        self.currency = currency 
+
     def times(self, multiplier):
-        return Franc(self.amount * multiplier)
+        return Money.franc(self.amount * multiplier)
    
-    def currency(self):
-        return "CHF"
+    def get_currency(self) -> str:
+        return self.currency
 
 
--- test_money_example.py
+++ test_money_example.py
@@ -26,5 +26,5 @@ def test_franc_multiplication():
 
 
 def test_currency():
-    assert "USD" == Money.dollar(1).currency()
-    assert "CHF" == Money.franc(1).currency()
+    assert "USD" == Money.dollar(1).get_currency()
+    assert "CHF" == Money.franc(1).get_currency()
