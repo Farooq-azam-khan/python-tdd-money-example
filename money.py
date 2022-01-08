eaea6e937e40d30f9faf9e355f7a787f4540f77e
Farooq <farooq1.khan@ryerson.ca>
2022-01-08 14:07:35 -0500

    testing currency concept

--- money.py
+++ money.py
@@ -16,20 +16,31 @@ class Money(ABC):
     def franc(amount: int):
         return Franc(amount)
     
+    
     @abstractmethod
     def times(self, multiplyer: int): # -> 'Money'
         pass 
+
+    
+    @abstractmethod
+    def currency(self): # -> 'str'
+        pass 
+
 
 class Dollar(Money):
    
     def times(self, multiplyer: int):
         return Dollar(self.amount * multiplyer)
-
+    
+    def currency(self):
+        return "USD"
 
 class Franc(Money):
 
     def times(self, multiplier):
         return Franc(self.amount * multiplier)
-    
+   
+    def currency(self):
+        return "CHF"
 
 
--- test_money_example.py
+++ test_money_example.py
@@ -23,3 +23,8 @@ def test_franc_multiplication():
     five = Money.franc(5)
     assert Money.franc(10) == five.times(2)
     assert Money.franc(15) == five.times(3)
+
+
+def test_currency():
+    assert "USD" == Money.dollar(1).currency()
+    assert "CHF" == Money.franc(1).currency()
