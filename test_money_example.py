bf947037a76d57a6c8b7de8a3688d34e1809b0a6
Farooq <farooq1.khan@ryerson.ca>
2022-01-08 13:12:33 -0500

    note on test_multiplication

--- test_money_example.py
+++ test_money_example.py
@@ -5,7 +5,7 @@ def test_multiplication():
     # Dollar to Dollar test
     assert Dollar(10) == five.times(2) 
     assert Dollar(15) == five.times(3) 
-
+    # Note: above test is easy to read as if it were assertions of truth rather than sequences of operations
 def test_equality():
     assert Dollar(5).equals(Dollar(5))
     # Triangulation: when the second test demands a more general solution, then and only then do we generalize
