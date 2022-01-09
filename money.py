a404620d6791ad2b1e5f4ce030d04ddbe5011826
Farooq <farooq1.khan@ryerson.ca>
2022-01-09 11:18:12 -0500

    moved Pair class to pair.py file

--- money.py
+++ money.py
@@ -1,15 +1,5 @@
 from abc import ABC, abstractmethod
- 
-class Pair:
-    def __init__(self, frm: str, to: str):
-        self.frm: str = frm 
-        self.to: str = to 
-
-    def __eq__(self, other):
-        return self.frm == other.frm and self.to == other.to
-
-    def __hash__(self):
-        return 0
+from pair import Pair 
 
 class Expression(ABC):
     @abstractmethod
--- test_money_example.py
+++ test_money_example.py
@@ -1,28 +1,38 @@
 from money import (Money, 
                    Bank, 
                    Expression, 
-                   Sum, 
-                   Pair)
+                   Sum)
+from pair import Pair 
 
 import pytest 
 
 
 def test_multiplication():
     five = Money.dollar(5)
-    # Dollar to Dollar test
     assert Money.dollar(10) == five.times(2) 
     assert Money.dollar(15) == five.times(3) 
-    # Note: above test is easy to read as if it were assertions of truth rather than sequences of operations
 
 
 def test_equality():
     assert Money.dollar(5) == Money.dollar(5)
-    # Triangulation: when the second test demands a more general solution, then and only then do we generalize
     assert Money.dollar(5) != Money.dollar(6)
     assert Money.franc(5) == Money.franc(5)
     assert Money.franc(5) != Money.franc(6)
-    # dollars and franc comparison
     assert Money.franc(5) != Money.dollar(5)
+
+
+@pytest.fixture
+def bank():
+    return Bank()
+
+@pytest.fixture
+def five_bucks():
+    return Money.dollar(5)
+
+
+@pytest.fixture
+def ten_francs():
+    return Money.franc(10)
 
 
 def test_franc_multiplication():
@@ -41,20 +51,6 @@ def test_repr():
 
 def test_different_class_equality():
     assert Money(10, "CHF") == Money.franc(10)
-
-
-@pytest.fixture
-def bank():
-    return Bank()
-
-@pytest.fixture
-def five_bucks():
-    return Money.dollar(5)
-
-
-@pytest.fixture
-def ten_francs():
-    return Money.franc(10)
 
 
 def test_simple_addition(bank, five_bucks):
